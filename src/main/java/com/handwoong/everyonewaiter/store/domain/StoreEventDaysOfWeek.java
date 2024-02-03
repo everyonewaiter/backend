828a4963fd2d7b2d6cd177bd256a3c66eee20d0d
Handwoong <handwoong@gmail.com>
2024-02-03 18:46:36 +0900

    feat: implement validation for duplicated days of week

--- src/main/java/com/handwoong/everyonewaiter/store/domain/StoreEventDaysOfWeek.java
+++ src/main/java/com/handwoong/everyonewaiter/store/domain/StoreEventDaysOfWeek.java
@@ -1,13 +1,24 @@
 package com.handwoong.everyonewaiter.store.domain;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class StoreEventDaysOfWeek {
 
     private final List<DayOfWeek> daysOfWeek;
+
+    public StoreEventDaysOfWeek(final List<DayOfWeek> daysOfWeek) {
+        validate(daysOfWeek);
+        this.daysOfWeek = daysOfWeek;
+    }
+
+    private void validate(final List<DayOfWeek> daysOfWeek) {
+        final HashSet<DayOfWeek> uniqueDaysOfWeek = new HashSet<>(daysOfWeek);
+        if (uniqueDaysOfWeek.size() != daysOfWeek.size()) {
+            throw new IllegalArgumentException("요일은 중복될 수 없습니다.");
+        }
+    }
 
     public String toString(final String delimiter) {
         return daysOfWeek.stream()
