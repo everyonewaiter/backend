96a5a537017052284fa0f0306e2859827a819d8e
Handwoong <handwoong@gmail.com>
2024-02-05 22:28:59 +0900

    feat: add custom aggregate root domain event publisher (#17)
    * feat: add aggregate root abstract class
    
    * feat: add domain event publish feature
    
    * feat: extend root domain classes with abstract aggregate root

---
+++ src/main/java/com/handwoong/everyonewaiter/common/domain/DomainEventPublisher.java
@@ -0,0 +1,39 @@
+package com.handwoong.everyonewaiter.common.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class DomainEventPublisher {
+
+    private final ApplicationEventPublisher eventPub;
+
+    @Pointcut("within(@org.springframework.stereotype.Repository *)")
+    public void repository() {
+    }
+
+    @Pointcut("execution(* *..save*(..))")
+    public void saveMethod() {
+    }
+
+    @Pointcut("execution(* *..delete*(..))")
+    public void deleteMethod() {
+    }
+
+    @AfterReturning(value = "repository() && (saveMethod() || deleteMethod())")
+    public void invoke(final JoinPoint joinPoint) {
+        for (final Object arg : joinPoint.getArgs()) {
+            if (arg instanceof final AggregateRoot root) {
+                root.domainEvents().forEach(eventPub::publishEvent);
+                root.clearDomainEvents();
+            }
+        }
+    }
+}
---
+++ src/test/java/com/handwoong/everyonewaiter/common/domain/AggregateRootTest.java
@@ -0,0 +1,62 @@
+package com.handwoong.everyonewaiter.common.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
+
+import java.util.Collection;
+import org.junit.jupiter.api.Test;
+
+class AggregateRootTest {
+
+    @Test
+    void Should_ReturnEvent_When_RegisterEvent() {
+        // given
+        final AggregateRoot root =
+            mock(AggregateRoot.class, withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+
+        // when
+        final TestEvent event = root.registerEvent(new TestEvent("테스트 이벤트"));
+
+        // then
+        assertThat(event.name).isEqualTo("테스트 이벤트");
+    }
+
+    @Test
+    void Should_GetAllRegisteredEvent_When_DomainEvents() {
+        // given
+        final AggregateRoot root =
+            mock(AggregateRoot.class, withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+        root.registerEvent(new TestEvent("이벤트1"));
+        root.registerEvent(new TestEvent("이벤트2"));
+        root.registerEvent(new TestEvent("이벤트3"));
+
+        // when
+        final Collection<Object> events = root.domainEvents();
+
+        // then
+        assertThat(events).hasSize(3).extracting("name").contains("이벤트1", "이벤트2", "이벤트3");
+    }
+
+    @Test
+    void Should_RemoveAllRegisteredEvent_When_ClearDomainEvents() {
+        // given
+        final AggregateRoot root =
+            mock(AggregateRoot.class, withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+        root.registerEvent(new TestEvent("이벤트1"));
+        root.registerEvent(new TestEvent("이벤트2"));
+        root.registerEvent(new TestEvent("이벤트3"));
+
+        // when
+        root.clearDomainEvents();
+        final Collection<Object> events = root.domainEvents();
+
+        // then
+        assertThat(events).isEmpty();
+    }
+
+    private record TestEvent(String name) {
+
+    }
+}
