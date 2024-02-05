6d673c2bfe240c8d55984e73a32584d85d76273f
Handwoong <handwoong@gmail.com>
2024-02-05 19:25:29 +0900

    feat: add aggregate root abstract class

---
+++ src/main/java/com/handwoong/everyonewaiter/common/domain/AggregateRoot.java
@@ -0,0 +1,29 @@
+package com.handwoong.everyonewaiter.common.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class AggregateRoot {
+
+    private final List<Object> domainEvents = new ArrayList<>();
+
+    protected <T> T registerEvent(final T event) {
+        Assert.notNull(event, "Domain event must not be null");
+        this.domainEvents.add(event);
+        return event;
+    }
+
+    protected Collection<Object> domainEvents() {
+        return Collections.unmodifiableList(this.domainEvents);
+    }
+
+    protected void clearDomainEvents() {
+        this.domainEvents.clear();
+    }
+}
