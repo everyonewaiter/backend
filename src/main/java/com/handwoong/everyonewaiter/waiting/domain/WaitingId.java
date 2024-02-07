a2ac3805d90eaaf26654ee6a87d2a8b38a5ffd0a
Handwoong <handwoong@gmail.com>
2024-02-07 17:38:45 +0900

    feat: add waiting domain class

---
+++ src/main/java/com/handwoong/everyonewaiter/waiting/domain/WaitingId.java
@@ -0,0 +1,9 @@
+package com.handwoong.everyonewaiter.waiting.domain;
+
+public record WaitingId(Long value) {
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
---
+++ src/main/java/com/handwoong/everyonewaiter/waiting/domain/WaitingNotificationType.java
@@ -0,0 +1,8 @@
+package com.handwoong.everyonewaiter.waiting.domain;
+
+public enum WaitingNotificationType {
+    REGISTER,
+    READY,
+    CANCEL,
+    ENTRY,
+}
