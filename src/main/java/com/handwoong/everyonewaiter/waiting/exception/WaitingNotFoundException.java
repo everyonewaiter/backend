e5a2bfd2f2d99b1edbc61ad39c578bc9e23c0bce
Handwoong <handwoong@gmail.com>
2024-02-15 01:38:55 +0900

    feat: add waiting not found exception handler

---
+++ src/main/java/com/handwoong/everyonewaiter/waiting/exception/WaitingNotFoundException.java
@@ -0,0 +1,15 @@
+package com.handwoong.everyonewaiter.waiting.exception;
+
+import com.handwoong.everyonewaiter.common.exception.BaseException;
+import lombok.Getter;
+
+@Getter
+public class WaitingNotFoundException extends BaseException {
+
+    private final String resource;
+
+    public WaitingNotFoundException(final String message, final String resource) {
+        super(message);
+        this.resource = resource;
+    }
+}
