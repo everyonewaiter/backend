6f9e93f6823c37da1e1e0217aa98f31b310b0831
Handwoong <handwoong@gmail.com>
2024-02-15 01:33:03 +0900

    feat: allow waiting cancel endpoint

--- src/main/java/com/handwoong/everyonewaiter/common/config/security/uri/AccessAllowUri.java
+++ src/main/java/com/handwoong/everyonewaiter/common/config/security/uri/AccessAllowUri.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AccessAllowUri implements AllowUri {
     ROOT("/"),
+    WAITING_CANCEL("/api/waiting/cancel"),
     ;
 
     private final String uri;
