4cfaeff4419c7d2b8361afb40e6aa124c58727f6
Handwoong <handwoong@gmail.com>
2024-02-15 20:43:06 +0900

    feat: implement category repository layer

---
+++ src/main/java/com/handwoong/everyonewaiter/category/infrastructure/CategoryJpaRepository.java
@@ -0,0 +1,7 @@
+package com.handwoong.everyonewaiter.category.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryJpaRepository extends JpaRepository<CategoryEntity, Long> {
+
+}
