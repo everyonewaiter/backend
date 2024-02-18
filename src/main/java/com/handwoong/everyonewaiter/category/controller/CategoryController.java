8eafeca11f16423d5fb36de596ae64cd884da957
Handwoong <handwoong@gmail.com>
2024-02-18 19:18:59 +0900

    feat: add category update controller feature

--- src/main/java/com/handwoong/everyonewaiter/category/controller/CategoryController.java
+++ src/main/java/com/handwoong/everyonewaiter/category/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.handwoong.everyonewaiter.category.controller;
 
 import com.handwoong.everyonewaiter.category.controller.port.CategoryService;
 import com.handwoong.everyonewaiter.category.controller.request.CategoryCreateRequest;
+import com.handwoong.everyonewaiter.category.controller.request.CategoryUpdateRequest;
 import com.handwoong.everyonewaiter.category.domain.CategoryId;
 import com.handwoong.everyonewaiter.common.dto.ApiResponse;
 import jakarta.validation.Valid;
@@ -9,6 +10,7 @@ import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,11 @@ public class CategoryController {
         return ResponseEntity
             .created(URI.create(categoryId.toString()))
             .body(ApiResponse.success());
+    }
+
+    @PutMapping
+    public ResponseEntity<ApiResponse<Void>> update(@RequestBody @Valid final CategoryUpdateRequest request) {
+        categoryService.update(request.toDomainDto());
+        return ResponseEntity.ok(ApiResponse.success());
     }
 }
--- src/test/java/com/handwoong/everyonewaiter/medium/category/snippet/CategoryRequestSnippet.java
+++ src/test/java/com/handwoong/everyonewaiter/medium/category/snippet/CategoryRequestSnippet.java
@@ -22,4 +22,20 @@ public class CategoryRequestSnippet {
             .description("카테고리 아이콘 이름")
             .attributes(constraints("20자 이하"))
     );
+    public static final Snippet UPDATE_REQUEST = requestFields(
+        fieldWithPath("id")
+            .type(JsonFieldType.NUMBER)
+            .description("카테고리 ID"),
+        fieldWithPath("storeId")
+            .type(JsonFieldType.NUMBER)
+            .description("매장 ID"),
+        fieldWithPath("name")
+            .type(JsonFieldType.STRING)
+            .description("카테고리명")
+            .attributes(constraints("20자 이하")),
+        fieldWithPath("icon")
+            .type(JsonFieldType.STRING)
+            .description("카테고리 아이콘 이름")
+            .attributes(constraints("20자 이하"))
+    );
 }
