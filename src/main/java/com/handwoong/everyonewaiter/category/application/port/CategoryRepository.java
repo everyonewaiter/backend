8a7cf6fb8c1b6f5aed05e5a8617d763281395fa5
Handwoong <handwoong@gmail.com>
2024-02-18 18:54:30 +0900

    feat: add category find method to repository

--- src/main/java/com/handwoong/everyonewaiter/category/application/port/CategoryRepository.java
+++ src/main/java/com/handwoong/everyonewaiter/category/application/port/CategoryRepository.java
@@ -1,8 +1,11 @@
 package com.handwoong.everyonewaiter.category.application.port;
 
 import com.handwoong.everyonewaiter.category.domain.Category;
+import com.handwoong.everyonewaiter.category.domain.CategoryId;
 
 public interface CategoryRepository {
 
     Category save(Category category);
+
+    Category findByIdOrElseThrow(CategoryId id);
 }
---
+++ src/main/java/com/handwoong/everyonewaiter/category/exception/CategoryNotFoundException.java
@@ -0,0 +1,15 @@
+package com.handwoong.everyonewaiter.category.exception;
+
+import com.handwoong.everyonewaiter.common.exception.BaseException;
+import lombok.Getter;
+
+@Getter
+public class CategoryNotFoundException extends BaseException {
+
+    private final String resource;
+
+    public CategoryNotFoundException(final String message, final String resource) {
+        super(message);
+        this.resource = resource;
+    }
+}
