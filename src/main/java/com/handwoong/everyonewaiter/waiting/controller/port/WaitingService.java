1e0992a9a3812544173032b2e251f67550bf5c0a
Handwoong <handwoong@gmail.com>
2024-03-03 06:40:39 +0900

    feat: add waiting find by unique code service feature

--- src/main/java/com/handwoong/everyonewaiter/waiting/controller/port/WaitingService.java
+++ src/main/java/com/handwoong/everyonewaiter/waiting/controller/port/WaitingService.java
@@ -2,13 +2,17 @@ package com.handwoong.everyonewaiter.waiting.controller.port;
 
 import com.handwoong.everyonewaiter.store.domain.StoreId;
 import com.handwoong.everyonewaiter.user.domain.Username;
+import com.handwoong.everyonewaiter.waiting.domain.Waiting;
 import com.handwoong.everyonewaiter.waiting.domain.WaitingId;
 import com.handwoong.everyonewaiter.waiting.dto.WaitingCancel;
 import com.handwoong.everyonewaiter.waiting.dto.WaitingRegister;
+import java.util.UUID;
 
 public interface WaitingService {
 
 	int count(Username username, StoreId storeId);
+
+	Waiting findByStoreIdAndUniqueCode(StoreId storeId, UUID uniqueCode);
 
 	WaitingId register(WaitingRegister waitingRegister);
 
