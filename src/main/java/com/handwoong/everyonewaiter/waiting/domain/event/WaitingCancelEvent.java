723f4d577e5b20d8046f466581b9e99244d1162d
Handwoong <handwoong@gmail.com>
2024-03-13 21:46:45 +0900

    feat: add waiting event handlers

--- src/main/java/com/handwoong/everyonewaiter/waiting/domain/event/WaitingCancelEvent.java
+++ src/main/java/com/handwoong/everyonewaiter/waiting/domain/event/WaitingCancelEvent.java
@@ -2,8 +2,8 @@ package com.handwoong.everyonewaiter.waiting.domain.event;
 
 import com.handwoong.everyonewaiter.common.domain.PhoneNumber;
 import com.handwoong.everyonewaiter.store.domain.StoreId;
-import com.handwoong.everyonewaiter.waiting.domain.WaitingNotificationType;
+import com.handwoong.everyonewaiter.waiting.domain.WaitingNumber;
 
-public record WaitingCancelEvent(StoreId storeId, WaitingNotificationType notificationType, PhoneNumber phoneNumber) {
+public record WaitingCancelEvent(StoreId storeId, WaitingNumber number, PhoneNumber phoneNumber) {
 
 }
--- src/main/java/com/handwoong/everyonewaiter/waiting/domain/event/WaitingRegisterEvent.java
+++ src/main/java/com/handwoong/everyonewaiter/waiting/domain/event/WaitingRegisterEvent.java
@@ -1,8 +1,41 @@
 package com.handwoong.everyonewaiter.waiting.domain.event;
 
 import com.handwoong.everyonewaiter.common.domain.PhoneNumber;
+import com.handwoong.everyonewaiter.store.domain.LandlineNumber;
+import com.handwoong.everyonewaiter.store.domain.StoreId;
+import com.handwoong.everyonewaiter.store.domain.StoreName;
+import com.handwoong.everyonewaiter.waiting.domain.WaitingAdult;
+import com.handwoong.everyonewaiter.waiting.domain.WaitingChildren;
+import com.handwoong.everyonewaiter.waiting.domain.WaitingNumber;
+import com.handwoong.everyonewaiter.waiting.domain.WaitingTurn;
 import com.handwoong.everyonewaiter.waiting.dto.WaitingGenerateInfo;
+import java.util.UUID;
 
-public record WaitingRegisterEvent(WaitingGenerateInfo waitingGenerateInfo, PhoneNumber phoneNumber) {
+public record WaitingRegisterEvent(
+		WaitingGenerateInfo waitingGenerateInfo,
+		WaitingAdult adult,
+		WaitingChildren children,
+		UUID uniqueCode,
+		PhoneNumber phoneNumber
+) {
 
+	public StoreId storeId() {
+		return waitingGenerateInfo.storeId();
+	}
+
+	public StoreName storeName() {
+		return waitingGenerateInfo.storeName();
+	}
+
+	public LandlineNumber landlineNumber() {
+		return waitingGenerateInfo.landlineNumber();
+	}
+
+	public WaitingNumber number() {
+		return waitingGenerateInfo.number();
+	}
+
+	public WaitingTurn turn() {
+		return waitingGenerateInfo.turn();
+	}
 }
