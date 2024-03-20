298c040cdfc7750f0d7cf907b83fc2234a9cb0a1
Handwoong <handwoong@gmail.com>
2024-03-20 20:14:09 +0900

    feat: add method to update a menu

--- src/test/java/com/handwoong/everyonewaiter/menu/domain/MenuTest.java
+++ src/test/java/com/handwoong/everyonewaiter/menu/domain/MenuTest.java
@@ -1,6 +1,7 @@
 package com.handwoong.everyonewaiter.menu.domain;
 
 import static com.handwoong.everyonewaiter.util.Fixtures.aCategory;
+import static com.handwoong.everyonewaiter.util.Fixtures.aMenu;
 import static com.handwoong.everyonewaiter.util.Fixtures.aMenuOptionGroup;
 import static com.handwoong.everyonewaiter.util.Fixtures.aStore;
 import static com.handwoong.everyonewaiter.util.Fixtures.aUser;
@@ -9,6 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.handwoong.everyonewaiter.category.domain.Category;
 import com.handwoong.everyonewaiter.category.domain.CategoryId;
 import com.handwoong.everyonewaiter.menu.dto.MenuCreate;
+import com.handwoong.everyonewaiter.menu.dto.MenuUpdate;
 import com.handwoong.everyonewaiter.store.domain.Store;
 import com.handwoong.everyonewaiter.store.domain.StoreId;
 import com.handwoong.everyonewaiter.user.domain.User;
@@ -41,7 +43,7 @@ class MenuTest {
 	}
 
 	@Test
-	void Should_Create_When_Valid() {
+	void Should_Create_When_ValidMenuCreate() {
 		// given
 		final MenuName menuName = new MenuName("수비드 소고기 스테이크");
 		final MenuCreate menuCreate = MenuCreate.builder()
@@ -63,5 +65,32 @@ class MenuTest {
 
 		// then
 		assertThat(menu.getName()).isEqualTo(menuName);
+	}
+
+	@Test
+	void Should_Update_When_ValidMenuUpdate() {
+		// given
+		final Menu menu = aMenu().build();
+		final MenuName menuName = new MenuName("소고기 스테이크 (수비드)");
+		final MenuUpdate menuUpdate = MenuUpdate.builder()
+				.menuId(new MenuId(1L))
+				.storeId(new StoreId(1L))
+				.categoryId(new CategoryId(1L))
+				.name(menuName)
+				.description(new MenuDescription("부채살을 수비드 방식으로 조리하여 촉촉하고 부드러운 식감을 즐길수 있는 스테이크"))
+				.image("")
+				.price(new Money(29_900))
+				.spicy(new MenuSpicy(0))
+				.printBillInKitchen(true)
+				.status(MenuStatus.BASIC)
+				.label(MenuLabel.REPRESENT)
+				.optionGroups(new MenuOptionGroups(List.of(aMenuOptionGroup().build())))
+				.build();
+
+		// when
+		final Menu updatedMenu = menu.update(menuUpdate, testContainer.menuValidator);
+
+		// then
+		assertThat(updatedMenu.getName()).isEqualTo(menuName);
 	}
 }
